ecaf530ceba7a4cbdff9bfa5691153d817b77477
Naveen Kumar <nav007@gmail.com>
2016-09-03 11:28:55 +0530

    FAQs added

---
+++ 3DModelling.mm
@@ -0,0 +1,103 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="3D Modelling" FOLDED="false" ID="ID_1043183880" CREATED="1472796794111" MODIFIED="1472797106594" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="2.0">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Blender" POSITION="right" ID="ID_891280193" CREATED="1472797652577" MODIFIED="1472834064155">
+<edge COLOR="#00007c"/>
+<node TEXT="Keyboard Shortcut" ID="ID_1278514843" CREATED="1472872757737" MODIFIED="1472872764020"/>
+<node TEXT="FAQs" FOLDED="true" ID="ID_824250249" CREATED="1472881891355" MODIFIED="1472881895277">
+<node TEXT="Group Objects" FOLDED="true" ID="ID_521504952" CREATED="1472881896380" MODIFIED="1472881906094">
+<node TEXT="Select all the object&#xa;you want to group" ID="ID_568659827" CREATED="1472881922778" MODIFIED="1472881947864"/>
+<node TEXT="Press ctrl+G" FOLDED="true" ID="ID_628176052" CREATED="1472881963242" MODIFIED="1472881964426">
+<node TEXT="you will notice a green&#xa;outline appear around them" ID="ID_802636819" CREATED="1472881972197" MODIFIED="1472881989186"/>
+</node>
+<node TEXT="Now in future, if you wish to&#xa;manipulate the whole group" FOLDED="true" ID="ID_899548226" CREATED="1472882006162" MODIFIED="1472882015099">
+<node TEXT="Select a member of the group and&#xa;then press shift+G and press Group" FOLDED="true" ID="ID_481106698" CREATED="1472882034590" MODIFIED="1472882059837">
+<node TEXT="it will select every other object in that group&#xa;which you can now drag around, scale etc" ID="ID_154011805" CREATED="1472882073622" MODIFIED="1472882095744"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
